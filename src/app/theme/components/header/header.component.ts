1fed1341339c5df0431451e0c509ffe5b63b9ff3
martinezdylan <martinez.dylanj@gmail.com>
2021-05-18 22:24:13 -0500

    add series of proof of concepts

--- src/app/theme/components/header/header.component.ts
+++ src/app/theme/components/header/header.component.ts
@@ -8,6 +8,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public solutions: Array<any> = [
+    {
+      svg: 'sp',
+      title: 'Strategic Programs',
+      description: 'Ongoing partnerships designed to identify and pursue strategic opportunities and empower growth.',
+      button: 'Learn More',
+      path: '#'
+    },
+    {
+      svg: 'sp',
+      title: 'Strategic Programs',
+      description: 'Ongoing partnerships designed to identify and pursue strategic opportunities and empower growth.',
+      button: 'Learn More',
+      path: '#'
+    },
+    {
+      svg: 'sp',
+      title: 'Strategic Programs',
+      description: 'Ongoing partnerships designed to identify and pursue strategic opportunities and empower growth.',
+      button: 'Learn More',
+      path: '#'
+    },
+  ]
 
   constructor(private _router: Router) { 
     
